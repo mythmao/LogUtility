d5146220794bea5ecd75a92476dcdfbb0dfb74a7
maocaiming <maocaiming@ucredit.com>
2015-04-08 14:09:15 +0800

    1

---
+++ Common/LogHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+namespace Common
+{
+    public class LogHelper
+    {
+        /// <summary>
+        /// 默认从LogUtinity.config初始化
+        /// </summary>
+        public LogHelper()
+        {
+            string str = System.Environment.CurrentDirectory;
+            string config = Path.Combine(str, "Common.config");
+            FileInfo fi = new FileInfo(config);
+            log4net.Config.XmlConfigurator.Configure(fi);
+        }
+        /// <summary>
+        /// 从App.config配置初始化
+        /// </summary>
+        /// <param name="selfConfig">是否从程序App.config加载配置</param>
+        public LogHelper(bool selfConfig)
+        {
+            if (selfConfig)
+            {
+                log4net.Config.XmlConfigurator.Configure();
+            }
+        }
+        public  void WriteLog(Type t, Exception ex)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(t);
+            log.Error("Error", ex);
+        }
+        public  void WriteLog(Type t, string msg)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(t);
+            log.Info(msg);
+        }
+        public  void WriteLog(string msg)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger("MyLogger");
+            log.Info(msg);
+            
+        }
+    }
+}
---
+++ LogUtinity/Program.cs
@@ -0,0 +1,66 @@
+﻿using Common;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogUtinity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string ConnStr = ConfigurationManager.ConnectionStrings["test"].ConnectionString;
+            LogHelper loghelper = new LogHelper(true);
+            loghelper.WriteLog("Hello,World!");
+            #region MySqlTest
+            //using (MySqlConnection conn = new MySqlConnection(ConnStr))
+            //{
+            //    conn.Open();
+            //    string sql = "INSERT INTO Log (Date,Thread,Level,Logger,Message,Exception) VALUES (@log_date, @thread, @log_level, @logger, @message, @exception)";
+            //    MySqlCommand cmd = conn.CreateCommand();
+            //    cmd.CommandText = sql;
+            //    {
+            //        MySqlParameter para = new MySqlParameter("log_date", MySqlDbType.DateTime);
+            //        para.Value = DateTime.Now;
+            //        cmd.Parameters.Add(para);
+            //    }
+            //    {
+            //        MySqlParameter para = new MySqlParameter("thread", MySqlDbType.String);
+            //        para.Value = "test";
+            //        cmd.Parameters.Add(para);
+            //    }
+            //    {
+            //        MySqlParameter para = new MySqlParameter("log_level", MySqlDbType.String);
+            //        para.Value = "info";
+            //        cmd.Parameters.Add(para);
+            //    }
+            //    {
+            //        MySqlParameter para = new MySqlParameter("logger", MySqlDbType.String);
+            //        para.Value = "test";
+            //        cmd.Parameters.Add(para);
+            //    }
+            //    {
+            //        MySqlParameter para = new MySqlParameter("message", MySqlDbType.String);
+            //        para.Value = "test";
+            //        cmd.Parameters.Add(para);
+            //    }
+            //    {
+            //        MySqlParameter para = new MySqlParameter("exception", MySqlDbType.String);
+            //        para.Value = "";
+            //        cmd.Parameters.Add(para);
+            //    }
+            //    int count = cmd.ExecuteNonQuery();
+            //    Console.WriteLine(count);
+            //    //object obj = cmd.ExecuteScalar();
+            //    //Console.WriteLine(Convert.ToInt32(obj));
+            //} 
+            #endregion
+            Console.WriteLine("Done!");
+            Console.ReadKey();
+        }
+    }
+}
